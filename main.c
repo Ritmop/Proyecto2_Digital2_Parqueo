a05bb66ae24ed2e41aa2738fd095d9bee76936c6
Ritmop <125711101+Ritmop@users.noreply.github.com>
2023-11-09 00:47:23 -0600

    Sensores LDR y UART
    Se agrego la lectura de sensores y la transmisión de parqueos disponibles hacia la computadora.

--- main.c
+++ main.c
@@ -37,13 +37,25 @@
 #define SENSORES_BASE  GPIO_PORTE_BASE
 #define LED_ROJO_BASE  GPIO_PORTD_BASE
 #define LED_VERDE_BASE GPIO_PORTC_BASE
-#define SENSORES_MASK  GPIO_PIN_3|GPIO_PIN_2|GPIO_PIN_1|GPIO_PIN_0
+#define SENSORES_PINS  GPIO_PIN_3|GPIO_PIN_2|GPIO_PIN_1|GPIO_PIN_0
 #define LED_ROJO_MASK  GPIO_PIN_3|GPIO_PIN_2|GPIO_PIN_1|GPIO_PIN_0
 #define LED_VERDE_MASK GPIO_PIN_7|GPIO_PIN_6|GPIO_PIN_5|GPIO_PIN_4
+#define UART2_PINS     GPIO_PIN_7|GPIO_PIN_6
 
-uint8_t sensorValues = 0x02;
+uint8_t sensorValues;
+uint8_t parqueosDisponibles;
+uint8_t parqueosOcupados;
 
+//**************************************************************************************************************
+// Prototipos de Función
+//**************************************************************************************************************
+void sendUART0(void);
+void sendUART2(void);
+uint8_t countSetBits(uint8_t bitwise);
 
+//**************************************************************************************************************
+// Código Principal
+//**************************************************************************************************************
 int main(void)
 {
     // configuración del reloj a 40MHz con el oscilador externo de 16MHz y PLL
@@ -51,7 +63,7 @@ int main(void)
 
     // Configurar Puerto E - Sensores
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
-    GPIOPinTypeGPIOInput(SENSORES_BASE, SENSORES_MASK);
+    GPIOPinTypeGPIOInput(SENSORES_BASE, SENSORES_PINS);
 
     // Configurar Puerto D - LEDS rojos
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);
@@ -61,17 +73,73 @@ int main(void)
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOC);
     GPIOPinTypeGPIOOutput(LED_VERDE_BASE, LED_VERDE_MASK);
 
-    //Habilitar módulo UART2
-    //SysCtlPeripheralEnable(SYSCTL_PERIPH_UART2);
+    //Habilitar módulo UART2 para la comunicación con ESP32
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART2);
+    GPIOPinTypeUART(GPIO_PORTD_BASE, UART2_PINS);
+    UARTConfigSetExpClk(UART2_BASE, SysCtlClockGet(), 115200,
+                        (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
+
+    //Habilitar módulo UART0 para la comunicación con computadora
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+    UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 115200,
+                        (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
+
+
 
     //**********************************************************************************************************
     // Loop Principal
     //**********************************************************************************************************
     while (1)
     {
-        GPIOPinWrite(LED_ROJO_BASE, LED_ROJO_MASK, sensorValues); //Sensor indica ocupado
-        GPIOPinWrite(LED_VERDE_BASE, LED_VERDE_MASK, (~sensorValues)<<4);
-        sensorValues++;
-        SysCtlDelay(90000000);
+        sensorValues = GPIOPinRead(SENSORES_BASE, SENSORES_PINS);   //Lectura de sensores
+        parqueosOcupados = sensorValues;            //Sensor tapado indica ocupado
+        parqueosDisponibles = (~sensorValues) & 0x0F;
+        GPIOPinWrite(LED_ROJO_BASE, LED_ROJO_MASK, parqueosOcupados);
+        GPIOPinWrite(LED_VERDE_BASE, LED_VERDE_MASK, parqueosDisponibles<<4); //Corrimiento debido a que los pines de los leds verdes son [7:4]
+
+        sendUART0();
+        SysCtlDelay(300000);
     }
+}
+
+//**********************************************************************************************************
+// Envio de datos a la computadora
+//**********************************************************************************************************
+void sendUART0(void){
+    uint8_t disponibles = countSetBits(parqueosDisponibles) + '0';
+    uint8_t ocupados = countSetBits(parqueosOcupados) + '0';
+    UARTCharPut(UART0_BASE,'D');
+    UARTCharPut(UART0_BASE,':');
+    UARTCharPut(UART0_BASE,disponibles);
+    UARTCharPut(UART0_BASE,' ');
+    UARTCharPut(UART0_BASE,'-');
+    UARTCharPut(UART0_BASE,' ');
+    UARTCharPut(UART0_BASE,'O');
+    UARTCharPut(UART0_BASE,':');
+    UARTCharPut(UART0_BASE,ocupados);
+    UARTCharPut(UART0_BASE,'\r');
+}
+
+//**********************************************************************************************************
+// Envio de datos a ESP32
+//**********************************************************************************************************
+void sendUART2(void){
+    uint8_t disponibles = countSetBits(parqueosDisponibles) + '0';
+    UARTCharPut(UART2_BASE,disponibles);
+    UARTCharPut(UART2_BASE,'\n');
+}
+
+uint8_t countSetBits(uint8_t bitwise){
+    uint8_t count = 0;
+    uint8_t i;
+    for (i = 0; i < 4; i++){
+        if ((bitwise % 2) == 1){
+            count++;
+        }
+        bitwise = bitwise >> 1;
+        bitwise &= 0x0F;
+    }
+    return count;
 }
